0d1db041c1d04156b125de966fca23428f821614
jake1104 <iamjake1104@gmail.com>
2026-01-01 10:56:22 +0900

    Update Content-Security-Policy for localhost URLs

--- app/main.py
+++ app/main.py
@@ -23,7 +23,9 @@ class CSPMiddleware(BaseHTTPMiddleware):
         response.headers["Content-Security-Policy"] = (
             "frame-ancestors "
             "http://localhost:5173 "
-            "http://localhost:8000"
+            "http://localhost:8000 "
+            "http://127.0.0.1:5173 "
+            "http://127.0.0.1:8000"
         )
         return response
 
