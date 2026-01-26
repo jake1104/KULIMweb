3b39eee0e3459c0e9b3c41f3bed9bdf16d82264a
jake1104 <iamjake1104@gmail.com>
2026-01-26 21:26:32 +0900

    Add files via upload

--- app/api.py
+++ app/api.py
@@ -2,8 +2,10 @@ from typing import Union
 from fastapi import APIRouter
 from pydantic import BaseModel
 from grammar import MorphAnalyzer, SyntaxAnalyzer
+from pronunciation import pronounce
+from romanization import romanize, romanize_standard
 
-morph_analyzer = MorphAnalyzer()
+morph_analyzer = MorphAnalyzer(use_rust=True, use_neural=True, use_gpu=True)
 syntax_analyzer = SyntaxAnalyzer()
 
 router = APIRouter(prefix="/api")
@@ -20,3 +22,20 @@ def morph_api(req: TextRequest):
 @router.post("/syntax")
 def syntax_api(req: TextRequest):
     return syntax_analyzer.analyze(text=req.text, morph_analyzer=morph_analyzer)
+
+
+@router.post("/pronunciation")
+def pronunciation_api(req: TextRequest):
+    return pronounce(req.text)
+
+
+@router.post("/romanization")
+def romanization_api(req: TextRequest):
+    return romanize(req.text)
+
+
+@router.post("/romanization_standard")
+def romanization_standard_api(req: TextRequest):
+    return romanize_standard(req.text)
+
+
--- app/embed.py
+++ app/embed.py
@@ -4,6 +4,21 @@ from pathlib import Path
 
 router = APIRouter()
 
+@router.get("/grammar_embed", response_class=HTMLResponse)
+def kulim_grammar_embed():
+    html = Path("static/grammar_embed.html").read_text(encoding="utf-8")
+    return html
+
+@router.get("/pronunciation_embed", response_class=HTMLResponse)
+def kulim_pronunciation_embed():
+    html = Path("static/pronunciation_embed.html").read_text(encoding="utf-8")
+    return html
+
+@router.get("/romanization_embed", response_class=HTMLResponse)
+def kulim_romanization_embed():
+    html = Path("static/romanization_embed.html").read_text(encoding="utf-8")
+    return html
+
 @router.get("/embed", response_class=HTMLResponse)
 def kulim_embed():
     html = Path("static/embed.html").read_text(encoding="utf-8")
